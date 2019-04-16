92e4258039276eb399cd79548cc44764adabc568
Dima Khomko <khomko.dima@gmail.com>
2019-04-16 13:26:09 +0300

    add

--- homework_1/scripts.js
+++ homework_1/scripts.js
@@ -1,14 +1,19 @@
 var money = +prompt("Ваш бюджет на месяц", "");
 var time = prompt("Введите дату в формате YYYY-MM-DD", "");
-var question1 = prompt("Введите обязательную статью расходов в этом месяце", "");
-var question2 = prompt("Во сколько обойдется?", "");
 
-var addData = {
+var appData = {
     budget: money,
     timeData: time,
-    expenses: {question1: question2},
+    expenses: {},
     optionalExpenses: null,
     income: null,
     savings: false, 
 };
-alert("Бюджет на 1 день сотовляет:" + addData["budget"] / 30);
+
+var question1 = prompt("Введите обязательную статью расходов в этом месяце", "");
+var question2 = prompt("Во сколько обойдется?", "");
+appData.expenses.question1 = question2;
+
+console.log(appData);
+
+// alert("Бюджет на 1 день сотовляет:" + addData["budget"] / 30);
