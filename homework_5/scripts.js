9735090fa4f1bcb5fcae91f5eaf9ea4e32cbc90b
Dima Khomko <khomko.dima@gmail.com>
2019-04-26 12:10:10 +0300

    homevork 5

--- homework_5/scripts.js
+++ homework_5/scripts.js
@@ -78,6 +78,6 @@ var appData = {
         });        
     }
 };
-for (let key in appData) {
+for (var key in appData) {
     console.log("Наша программа включает в себя данные: " + key + " - " + appData[key]);
 }
